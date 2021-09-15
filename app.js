4ab76347388cfe3b21c615d7f8a5ab4838435dcf
danny2327 <dstrood@gmail.com>
2021-09-15 09:49:54 -0700

    Have to put off the syncronous at this time, about to start making it work with buttons to move forward and back.  Added buttons already.

--- app.js
+++ app.js
@@ -1,56 +1,56 @@
 const http = new EasyHTTP;
 const display = document.getElementById('display');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+
 let grid;
 
-function drawTubes() {
+function prepareToDraw() {
     ballsPerTube = grid[0][0].length//getTubeSize()
     numberOfTubes = grid[0].length
-    // console.log(`ballsPerTube: ${ballsPerTube}, numberOfTubes: ${numberOfTubes}`);
-    //for each tube in the game (the length of any one of grid[x])
-    // draw a tube
-    // for each ball in this tube, draw that many balls. 
-    // and in that tube draw the correct number of balls. 
-
-    
 
     for (let currentStage = 0;currentStage < grid.length;currentStage++) {
-        console.log(currentStage);
-        //displays the tubes
-        for(let i = 0; i < numberOfTubes; i++) {
-            let tubeDiv =  document.createElement('div');
-            tubeDiv.className = 'tube';
-            // displays the balls
-            //done in reverse so we're effectively drawing from the bottom up
-            for (let x = grid[currentStage][i].length-1; x > -1 ; x--) {
-                // console.log(grid[currentStage][i][x]);
-                //create ball (div), set class and color
-                let ball = document.createElement('div');
-                ball.className = "ball";
-                ball.style.backgroundColor = grid[currentStage][i][x];
-                // add the ball to the tube
-                tubeDiv.appendChild(ball);
-            }
-            // console.log('----');            
-            
-            display.appendChild(tubeDiv)
-        }
+        drawTubes(currentStage);
         display.appendChild(document.createElement("p"));
-        setTimeout(
-            function() { 
-                console.log(display);
-                // alert("going away now");
-                display.innerText='..';
-            },
-            2500);
+        delay(currentStage);
+    }
+}
+
+function delay(currentStage){
+    setTimeout(() => {
+            display.innerText='..';
+            console.log(currentStage);
+            drawTubes(currentStage);
+        }, 2500);
+}
+
+function drawTubes(currentStage){
+    //displays the tubes
+    for(let i = 0; i < numberOfTubes; i++) {
+        let tubeDiv =  document.createElement('div');
+        tubeDiv.className = 'tube';
+        // displays the balls
+        //done in reverse so we're effectively drawing from the bottom up
+        for (let x = grid[currentStage][i].length-1; x >= 0 ; x--) {
+            // console.log(grid[currentStage][i][x]);
+            //create ball (div), set class and color
+            let ball = document.createElement('div');
+            ball.className = "ball";
+            ball.style.backgroundColor = grid[currentStage][i][x];
+            // add the ball to the tube
+            tubeDiv.appendChild(ball);
+        }
+        // console.log('----');            
+        
+        display.appendChild(tubeDiv)
     }
 }
     
-    http.get('ballsortSolved.json')
+http.get('ballsortSolved.json')
 .then(data => displayData(data))
 .catch(err => console.log(err)) 
 
 function displayData(data) {
     grid = Object.values(data);
-    // console.log(grid);
-    drawTubes()
+    prepareToDraw()
 }
