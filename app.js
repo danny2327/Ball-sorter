e229dd9ef068427d4f919442efd0e0d2b58668c7
danny2327 <dstrood@gmail.com>
2021-09-14 22:49:36 -0700

    Tubes show up with coloured balls, though they're not in the right spot or right number.

--- app.js
+++ app.js
@@ -8,22 +8,36 @@ function drawTubes() {
     // console.log(`ballsPerTube: ${ballsPerTube}, numberOfTubes: ${numberOfTubes}`);
     let i=0;
     let x=0;
-    for(tube in grid){
-        console.log(grid[tube]);
+    //for each tube in the game (the length of any one of grid[x])
+    // draw a tube
+    // for each ball in this tube, draw that many balls. 
+    // and in that tube draw the correct number of balls. 
+
+    for(let i = 0; i < numberOfTubes; i++) {
         let tubeSpan =  document.createElement('span');
         tubeSpan.className = 'tube';
         let balls = ''
-        for (balls in grid[tube]) {
-            balls += `<div class="ball">X</div>`;            
+        for (x = 0; x < ballsPerTube; x++) {
+            for (y=0;y < grid[i][x].length; y++) {
+                // console.log(y); 
+                // console.log(grid[i][x][y]);
+                let ball = document.createElement('div');
+                ball.className = "ball";
+                ball.innerText = "X";
+                ball.style.backgroundColor = grid[0][x][y];
+                console.log(ball);
+                tubeSpan.appendChild(ball);
+            }
+            // console.log('---');
+
+            // balls += `<div class="ball">X</div>`;    
+            // console.log(grid[i][x]);        
         }
-        tube.innerHTML = balls;
+        // tubeSpan.innerHTML = balls;
         display.appendChild(tubeSpan)
         // console.log(tube);
     }
 }
-
-
-
 
 http.get('ballsortSolved.json')
 .then(data => displayData(data))
