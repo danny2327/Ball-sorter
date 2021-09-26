6d8b87aa542aabb7b3933d3b2b7ef8e890b39d15
danny2327 <dstrood@gmail.com>
2021-09-25 23:19:11 -0700

    Got the builder mostly working, not saving anything yet though.  Last problem to tackle is the border not disappearing

--- js/builder.js
+++ js/builder.js
@@ -2,24 +2,28 @@ let inputBalls = document.getElementById('numBalls');
 let inputColours = document.getElementById('numColours');
 const builderTubeDisplay = document.getElementById('builderTubeDisplay');
 const builderSelectorDisplay = document.getElementById('builderSelectorDisplay');
+let ballSelector = document.getElementById('ballSelect');
+let currentBallPosition = [0,0];
+let currentBall;
 
 let builderBallsPerTube;
 let builderNumberOfTubes;
 let builderGrid;
+let full = false;
 
-function createGrid() {
-    let newGrid = {}
-    for (let i=0; i < (builderNumberOfTubes); i++) {
-        newGrid[i] = {};
-        for (let b=0; b < builderBallsPerTube; b++) {
-            if (i < (builderNumberOfTubes-2)) {    
-                newGrid[i][b] = '';
-            }
-        }
-    }
-    console.log(newGrid);
-    return newGrid;
-}
+// function createGrid() {
+//     let newGrid = {}
+//     for (let i=0; i < (builderNumberOfTubes); i++) {
+//         newGrid[i] = {};
+//         for (let b=0; b < builderBallsPerTube; b++) {
+//             if (i < (builderNumberOfTubes-2)) {    
+//                 newGrid[i][b] = '';
+//             }
+//         }
+//     }
+//     console.log(newGrid);
+//     return newGrid;
+// }
 
 document.getElementById('updateGrid').addEventListener('click', (e) => {
     if (confirm('Are you sure you want to change the size and reset?')) {
@@ -34,6 +38,20 @@ document.getElementById('updateGrid').addEventListener('click', (e) => {
       }
 });
 
+ballSelector.addEventListener('click', (e) => {
+    if (!full) {
+        if(e.path[1].className == 'ball') {
+            console.log('before update')
+            updateBall(e.path[1].style.backgroundColor)
+            console.log('after update')
+            nextBall();
+            console.log('after next')
+        }
+    } else {
+        console.log('I do believe you\'re done')
+    }
+});
+
 function getInputs() {
     builderNumberOfTubes = (parseInt(inputColours.value) + 2);
     builderBallsPerTube = inputBalls.value;
@@ -42,12 +60,10 @@ function getInputs() {
 function prepareToDraw() {
     builderTubeDisplay.innerHTML='';
     getInputs();
-    builderGrid = createGrid();
+    // builderGrid = createGrid();
     drawTubes();
     drawBallSelector();
 }
-
-
 
 function createTube(i) {
     tubeDiv = document.createElement('div');
@@ -82,12 +98,11 @@ function drawTubes(){
         for (let x = builderBallsPerTube-1; x >= 0 ; x--) {
             
             if(i < builderNumberOfTubes-2) {
-
                 let ball = createBall();
                 // add the ball to the tube
                 tubeDiv.appendChild(ball);
                 // ball.style.backgroundColor = grid[currentStage][i][x];
-                ballBottom = 0 + (x * 32)
+                ballBottom = 0 + (x * 34)
                 ball.style.bottom = ballBottom + "px";
             }
         }        
@@ -96,11 +111,9 @@ function drawTubes(){
 }
 
 function drawBallSelector() {
-    let ballSelector = document.getElementById('ballSelect');
     ballSelector.innerHTML = '';
     ballSelector.style.width = parseInt(inputColours.value)*65;
     ballSelector.style.height = 35;
-    console.log(50+(32*builderBallsPerTube));
     ballSelector.style.top = 80+(32*builderBallsPerTube)+"px";
     for (let i = 0; i < parseInt(inputColours.value); i++) {
         let ball = createBall()
@@ -115,13 +128,66 @@ function drawBallSelector() {
 function startBuild() {
     // This is where I'll do the loop of highlighting the next ball, up one row to the next etc.
     // allow clicking on any ball to set/redo. 
-
+    setBall();
+    selectBall();
+    // nextBall();
+    // for(let i = 0; i < builderNumberOfTubes; i++) {
+        // for (let x = 0; x < builderBallsPerTube; x++) {
+            //ignore the last 2 empty tubes
+    // if(currentBall[0] < builderNumberOfTubes-2) {
+    // }
 }
 
+function resetBorder() {
+    currentBall.style.border = 'border: 1px solid grey';
+}
 
-function displayTubes() {
+function setBall() {
+    balls = getBalls();
+    currentBall = balls[currentBallPosition[1]]
+}
+
+function getBalls() {
+    tubes = getTubes();
+    tube = tubes[currentBallPosition[0]]
+    return tube.querySelectorAll('.ball');
+}
+
+function getTubes() {
+    return  document.querySelectorAll('.tube');
+}
+
+function selectBall() {
+    currentBall.style.border = '2px solid red'
+}
+
+function updateBall(newColour) {
+    currentBall.style.backgroundColor = newColour;
+}
+
+function nextBall() {
+    let tube = currentBallPosition[0];
+    let ball = currentBallPosition[1];
+    // if not done in tube, next ball
+    if (ball < builderBallsPerTube-1) {
+        currentBallPosition[1]++
+    // if last tube is done, next tube, back to place 0         
+    } else if (tube < builderNumberOfTubes-3) {
+        currentBallPosition[1] = 0;
+        currentBallPosition[0]++;
+    } else {
+        console.log(currentBallPosition);
+        console.log(full);
+        //else all have been filled
+        full = true;
+    }
+    setBall();
+    selectBall();
+}
+
+function main() {
     prepareToDraw();
     startBuild();
 }
 
-displayTubes();
+main();
