fc2641fa25aaa9d2601ec3eacf02136fbbf1c391
danny2327 <dstrood@gmail.com>
2021-10-11 01:36:07 -0700

    In the middle
    Not done or even working in any way but I've done a lot.  Solver.js is empty

--- js/app.js
+++ js/app.js
@@ -1,63 +1,429 @@
+class Test {
+    constructor() {
+        // const fart = 'smelly';
+        this.fart = 'smelly';
+        // console.log(this.fart);
 
+    }
 
+    makeSound() {
+        console.log("pftttttteeet");
+    }
+}
+
+// class Test2 {
+//     constructor(test) {
+//         test.makeSound();
+//     }
+// }
+// 
+let test = new Test();
+// let test2 = new Test2(test);
 
 class App{
     constructor() {
-        this.solver = new Solver();
-        this.builder = new Builder();
+        this.ballColours = {
+            0: 'RED',
+            1: 'BLUE',
+            2: 'YELLOW',
+            3: 'LIME',
+            4: 'PURPLE',
+            5: 'ORANGE',
+            6: 'AQUA',
+            7: 'TEAL',
+            8: 'GREEN',
+            9: 'WHITE',
+            10: 'FUCHSIA',
+            11: 'LIGHTPINK',
+            12: 'BROWN',
+            13: 'MIDNIGHTBLUE'
+        };
+                
+        this.solverTab = document.getElementById('solver');
+        this.builderTab = document.getElementById('builder');
+        this.lnkSolver = document.getElementById('lnkSolver');
+        this.lnkBuilder = document.getElementById('lnkBuilder');
+        
+        this.lnkSolver.addEventListener('click', () => {
+            this.hideBuilder();
+            this.showSolver();
+        });
+        
+        this.lnkBuilder.addEventListener('click', () => {
+            this.hideSolver();
+            this.showBuilder();
+            
+        });
+        
+        this.solver = new Solver(this.ballColours);
+        this.builder = new Builder(this.ballColours);
+
+        // Hides Builder initially
+        // hideSolver();
+        this.hideBuilder();
+    }
+    showSolver() {
+        this.solverTab.style.display = 'block';
+    }
+
+    showBuilder() {
+        this.builderTab.style.display = 'block';
+    }
+
+    hideSolver() {
+        this.solverTab.style.display = 'none';
+        this.showBuilder();
+    }
+
+    hideBuilder() {
+        this.builderTab.style.display = 'none';
+        this.showSolver();
     }
 }
 
 
 class Solver {
-    constructor() {
+    constructor(ballColours) {
+        this.display = document.getElementById('display');
         
+        this.prev = document.getElementById('prev');
+        this.next = document.getElementById('next');
+
+        this.stage = document.createElement('span');
+
+
+        this.addEventListeners();
+
+        this.prev.disabled = "true";
+        
+        
+        // **Variables**
+        this.tubes = {}
+        this.numberOfTubes;
+        this.ballsPerTube;
+        this.http = new EasyHTTP;
+        this.grid;
+        this.ballColours = ballColours;
+        this.currentStage = 0;
+        this.playing = false;
+        //this.player = new Player();
+        this.timeoutHandle;
+        //default play speed in ms
+        this.playSpeed = 1000;
+
+        
+        // this.http.get('../Examples Solved/ball_sort_616_solved.json') //  14 colours x 5, puzzle 616 
+        // this.http.get('../Examples Solved/ballsortSolved.json') //   3 colours x 3
+        // this.http.get('../Examples Solved/ballSortSolved7x4.json') //   7 colours x 4
+        // this.http.get('../Examples Solved/ballSortSolved3x4.json') //   3 colours x 4
+        this.http.get('../Examples Solved/ballSortSolved12x5.json') 
+            .then(data => this.solve(data))
+            .catch(err => console.log(err)); 
+
+    } //End of Constructor
+
+    addEventListeners() {
+        document.querySelectorAll(".actionButton").forEach(button => 
+            button.addEventListener('click', (e) => {
+                switch(e.target.id) {
+                    case 'beginning':
+                        this.firstStage();
+                        break;
+                    case 'prev':
+                        this.prevStage();
+                        break;
+                    case 'next':
+                        this.nextStage();
+                        break;
+                    case 'last':
+                        this.lastStage();
+                        break;
+                    case 'play':
+                        this.play();
+                        break;
+                    case 'playFromStart':
+                        this.playFromStart();
+                        break;
+                    case 'pause':
+                        this.pause();
+                        break;
+                    case 'slower':
+                        this.slower();
+                        break;
+                    case 'faster':
+                        this.faster();
+                        break;
+                    case 'reset':
+                        this.reset();
+                        break;
+                }
+        }));
+        
+        document.addEventListener('keydown', (e) => {
+            const key = e.code
+            switch(key) {
+                case "ArrowLeft": {
+                    this.prevStage();
+                    break;
+                }
+                case "ArrowRight": {
+                    this.nextStage();
+                    break;
+                }
+                case "ArrowUp": {
+                    this.firstStage();
+                    break;
+                }
+                case "ArrowDown": {
+                    this.lastStage();
+                    break;
+                }
+                case "Space": {
+                    this.playPause();
+                    break;
+                }
+            }
+        });
     }
+
+    solve(data) {
+        this.grid = Object.values(data);
+        //look at the number of tubes in the first grid
+        this.numberOfTubes = this.grid[0].length
+        //look at the number of balls in the first tube in the first grid.
+        this.ballsPerTube = this.grid[0][0].length;//getTubeSize()
+        this.prepareToDraw();
+    }
+
+
+    prepareToDraw() {
+        this.setDisplaySize();
+        this.drawTubes();
+    }
+
+    setDisplaySize() {
+        this.display.style.width = (60+(32*this.ballsPerTube)) + "px";
+    }
+
+    drawTubes(){
+        //displays the tubes
+        this.stage.innerText = `Stage ${this.currentStage+1} of ${this.grid.length}`;
+        //reset Tubes
+        this.tubes = {};
+        for(let i = 0; i < this.numberOfTubes; i++) {
+            let tube = new Tube(this.ballsPerTube)
+            tube.setLeft(i);
+            tube.setTop(100);
+            this.tubes[i] = tube;
+            
+            //want to add - make the from and destination tubes change colour
+            this.display.appendChild(tubeDiv);        
+            // displays the balls
+            //done in reverse so we're effectively drawing from the bottom up
+            for (let x = grid[currentStage][i].length-1; x >= 0 ; x--) {
+                //create ball (div), set class and color
+                let ball = new Ball(grid[currentStage][i][x]);  
+                ball.setBottom(x * 32)
+                tube.addBall(ball);
+            }        
+        }
+        display.appendChild(document.createElement("p"));
+    }
+
+    prevStage() {
+        if(currentStage > 0) currentStage--;
+        if(currentStage == 0) prev.disabled = true;
+        next.disabled = false;
+        display.innerHTML = '';
+        drawTubes();
+    }
+
+    nextStage() {
+        if(currentStage < grid.length-1) currentStage++;
+        if(currentStage == grid.length-1) next.disabled = true;
+        prev.disabled = false;
+        display.innerHTML = '';
+        drawTubes();
+    }
+    
+    firstStage() {
+        currentStage = 0;
+        prev.disabled = true;
+        next.disabled = false;
+        display.innerHTML = '';
+        drawTubes();
+    }
+    
+    lastStage() {
+        currentStage = grid.length-1
+        next.disabled = true;
+        prev.disabled = false;
+        display.innerHTML = '';
+        drawTubes();
+    }
+    
+    async play() {
+        resetTimeout();
+        const wait = (timeToDelay) => new Promise((resolve) => timeoutHandle = setTimeout(resolve, timeToDelay));
+        
+        ShowPlayControls();
+        while (currentStage < grid.length-1) {
+            await wait(playSpeed);
+            nextStage();
+        }
+        hidePlayControls();
+    }
+    
+    playPause() {
+        //if playing, pauses, if not, starts 
+        if (isPlaying()) pause();
+        else play();
+    }
+    
+    isPlaying() {
+        if (document.querySelector('.playControls').style.visibility == 'hidden') {
+            return false; 
+        } else {
+            return true;
+        }
+    }
+    
+    playFromStart() {
+        resetTimeout();
+        currentStage = -1;
+        play();
+    }
+    
+    pause() {
+        resetTimeout();
+        hidePlayControls();
+    }
+    
+    resetTimeout() {
+        clearTimeout(timeoutHandle);
+    }
+    
+    faster() {
+        // don't want to be able to stop it
+        if (playSpeed > 200) playSpeed-=200;
+        console.log(playSpeed);
+    }
+    
+    slower() {
+        playSpeed+=200;
+        console.log(playSpeed);
+    }
+    
+    reset() {
+        playSpeed = 1000;
+        console.log(playSpeed);
+    }
+    
+    ShowPlayControls() {
+        document.querySelectorAll('.playControls').forEach((el) => el.style.visibility = 'visible');  
+    }
+    
+    hidePlayControls() {
+        document.querySelectorAll('.playControls').forEach((el) => el.style.visibility = 'hidden');
+    }
+    
 }
 
 class Builder {
-    constructor() {
-        
-    }
-}
+    constructor(ballColours) {
 
-class SolverUI {
-    constructor() {
-        prev.disabled = "true";
-        determineDisplaySize();
-    }     
-
-    determineDisplaySize() {
-        // newTubeHeight = 30+(32*this.ballsPerTube);
-        // display.style.width = (60+(32*ballsPerTube)) + "px";
-    }
-}
-
-class BuilderUI {
-    constructor() {
-        
+        // **Variables**
+        this.ballColours = ballColours;
     }
 }
 
 class Ball {
-    constructor(colour = null, position = null, tube = null) {
+    constructor(colour = null) {
         this.colour = colour;
-        this.position = position;
-        this.tube = tube;
+
+        this.ballDiv = this.getDiv();
+        return this;
     }
 
-    get colour() {}
-    
-    changeColour() { }
+    getDiv() {
+        let div = document.createElement('div');
+        div.className = "ball";
+        div.setColour(colour);
+        return div;
+    }
 
-    clearColour() { }
+    setColour(colour) {
+        if (colour) {
+            this.ballDiv.style.backgroundImage = `radial-gradient(at bottom right, white 10%, ${colour} 80%)`;
+            this.ballDiv.style.backgroundRepeat = "no-repeat";
+            this.colour = colour;
+        } else {
+            this.clearColour();
+        }
+    }
+
+    setBottom(ballBottom) {
+        this.ballDiv.style.bottom = ballBottom + "px";
+    }
+
+    get colour() {
+        return this.colour;
+    }
+
+    clearColour() {
+        this.ballDiv.style.backgroundImage = '';
+        this.colour = '';
+    }
 }
 
 class Tube {
-    constructor() {
+    constructor(ballsPerTube, i) {
+        this.ballsPerTube = ballsPerTube;
         this.balls = [];
+        setHeight(this.determineHeight());
+        this.tubeDiv = this.getDiv(); 
+        this.tubeDiv.className = 'tube';
+
+        this.setDivHeight();
     }
+
+    getDiv() {
+        let div = document.createElement('div');
+        div.className = 'tube';
+    }
+
+    determineHeight() {
+        return 30+(32*this.ballsPerTube)
+    }
+
+    addBall(ball) {
+        this.balls.push(ball);
+    }
+
+    getTubeDiv() {
+        return this.tubeDiv;
+    }
+
+    setHeight(height) {
+        this.height = height;
+        this.tubeDiv.style.height = this.height + "px";
+    }
+
+
+    //This shouldn't be in the tube class, it should be controlled by the controller (builder or solver)
+    // Won't even have to be once I get rid of the absolute stuff
+    setLeft(i) {
+        let tubeLeft = 30 + (60*i);
+        this.tubeDiv.style.left = tubeLeft + "px"; 
+    }
+
+    setTop(top) {
+        this.tubeDiv.style.top = top + "px";
+    }
+
+
 }
+
 
 // class grid {
 //     constructor(numberOfColours, numberOfBalls) {
@@ -67,25 +433,7 @@ class Tube {
 //     }
 // }
 
-
-
-const http = new EasyHTTP;
-const ballColours = {
-    0: 'RED',
-    1: 'BLUE',
-    2: 'YELLOW',
-    3: 'LIME',
-    4: 'PURPLE',
-    5: 'ORANGE',
-    6: 'AQUA',
-    7: 'TEAL',
-    8: 'GREEN',
-    9: 'WHITE',
-    10: 'FUCHSIA',
-    11: 'LIGHTPINK',
-    12: 'BROWN',
-    13: 'MIDNIGHTBLUE'
-};
+app = new App();
 
 // BLUE
 // RED
@@ -102,42 +450,8 @@ const ballColours = {
 // DARK PINK = FUCHSIA
 // LIGHT BLUE = AQUA
 
-// Make this project in this JS, and some framewords like angular, react, vue
-let solverTab = document.getElementById('solver');
-let builderTab = document.getElementById('builder');
-let lnkSolver = document.getElementById('lnkSolver');
-let lnkBuilder = document.getElementById('lnkBuilder');
+// Make this project in this JS, and some frameworks like angular, react, vue
 
-
-// Hides Builder initially
-// hideSolver();
-hideBuilder();
-
-lnkSolver.addEventListener('click', () => {
-    hideBuilder();
-    showSolver();
-});
-
-lnkBuilder.addEventListener('click', () => {
-    hideSolver();
-    showBuilder();
-});
-
-function showSolver() {
-    solverTab.style.display = 'block';
-}
-
-function showBuilder() {
-    builderTab.style.display = 'block';
-}
-
-function hideSolver() {
-    solverTab.style.display = 'none';
-}
-
-function hideBuilder() {
-    builderTab.style.display = 'none';
-}
 
 function createBall(colour = null) {
     //create ball (div), set class 
--- js/solver.js
+++ js/solver.js
@@ -1,250 +1,32 @@
-const display = document.getElementById('display');
-const prev = document.getElementById('prev');
-const stage = document.createElement('span');
 
 
-const next = document.getElementById('next');
-let currentStage = 0;
-let grid;
-let ballsPerTube;
-let playing = false;
-let timeoutHandle;
-//default play speed in ms
-let playSpeed = 1000;
 
-function  determineDisplaySize() {
-    newTubeHeight = 30+(32*this.ballsPerTube);
-    display.style.width = (60+(32*ballsPerTube)) + "px";
-}
 
-function prepareToDraw() {
-    numberOfTubes = grid[0].length
-    //disabled prev at start because it's 0
+
+
+
+// function prepareToDraw() {
+//     numberOfTubes = grid[0].length
+//     //disabled prev at start because it's 0
     
-    display.before(stage);
-    determineDisplaySize();
-    drawTubes();
-}
 
-// function determineDisplaySize() {
-//     newTubeHeight = 30+(32*ballsPerTube);
-//     display.style.width = (60+(32*ballsPerTube)) + "px";
+    // determineDisplaySize();
+    // drawTubes();
 // }
 
+
 //Want to fix: extract tubes from every loop
-function drawTubes(){
-    //displays the tubes
-    stage.innerText = `Stage ${currentStage+1} of ${grid.length}`;
-    for(let i = 0; i < numberOfTubes; i++) {
-        let tubeDiv =  document.createElement('div');
-        tubeDiv.className = 'tube';
-        newTubeDivLocation = 30 + (60*i);
-        newTubeHeight = 30+(32*ballsPerTube);
-        tubeDiv.style.height = newTubeHeight + "px";
-        tubeDiv.style.left = newTubeDivLocation + "px"; 
-        tubeDiv.style.top = 100 + "px";
-        //want to add - make the from and destination tubes change colour
-        display.appendChild(tubeDiv);        
-        // displays the balls
-        //done in reverse so we're effectively drawing from the bottom up
-        for (let x = grid[currentStage][i].length-1; x >= 0 ; x--) {
-            //create ball (div), set class and color
-            let ball = document.createElement('div');
-            ball.className = "ball";
-            ball.style.backgroundImage = `radial-gradient(at bottom right, white 10%, ${grid[currentStage][i][x]} 80%)`;
-            // ball.style.backgroundPosition = "55% 55%";
-            ball.style.backgroundRepeat = "no-repeat";
-            // let ballbg = document.createElement('img');
-            // ballbg.src='../images/ballbg.png';
-            // ballbg.className='ballbg';
-            // background-image:  radial-gradient(at bottom left, white 10%, rgba(30, 75, 115, 1) 40%);
-            // background-position: 55% 55%;
-            // background-repeat: no-repeat;
+// function 
 
-            // ball.appendChild(ballbg);
-            // ball.style.backgroundColor = grid[currentStage][i][x];
-            // add the ball to the tube
-            //stack balls from the bottom
-            tubeDiv.appendChild(ball);
 
-            ballBottom = 0 + (x * 32)
-            ball.style.bottom = ballBottom + "px";
-        }        
-    }
-    display.appendChild(document.createElement("p"));
-}
-
-document.querySelectorAll(".actionButton").forEach(button => 
-    button.addEventListener('click', (e) => {
-        switch(e.target.id) {
-            case 'beginning':
-                firstStage();
-                break;
-            case 'prev':
-                prevStage();
-                break;
-            case 'next':
-                nextStage();
-                break;
-            case 'last':
-                lastStage();
-                break;
-            case 'play':
-                play();
-                break;
-            case 'playFromStart':
-                playFromStart();
-                break;
-            case 'pause':
-                pause();
-                break;
-            case 'slower':
-                slower();
-                break;
-            case 'faster':
-                faster();
-                break;
-            case 'reset':
-                reset();
-                break;
-        }
-}));
-
-document.addEventListener('keydown', (e) => {
-    const key = e.code
-    switch(key) {
-        case "ArrowLeft": {
-            prevStage();
-            break;
-        }
-        case "ArrowRight": {
-            nextStage();
-            break;
-        }
-        case "ArrowUp": {
-            firstStage();
-            break;
-        }
-        case "ArrowDown": {
-            lastStage();
-            break;
-        }
-        case "Space": {
-            playPause();
-            break;
-        }
-    }
-});
     
-function prevStage() {
-    if(currentStage > 0) currentStage--;
-    if(currentStage == 0) prev.disabled = true;
-    next.disabled = false;
-    display.innerHTML = '';
-    drawTubes();
-}
-function nextStage() {
-    if(currentStage < grid.length-1) currentStage++;
-    if(currentStage == grid.length-1) next.disabled = true;
-    prev.disabled = false;
-    display.innerHTML = '';
-    drawTubes();
-}
-
-function firstStage() {
-    currentStage = 0;
-    prev.disabled = true;
-    next.disabled = false;
-    display.innerHTML = '';
-    drawTubes();
-}
-
-function lastStage() {
-    currentStage = grid.length-1
-    next.disabled = true;
-    prev.disabled = false;
-    display.innerHTML = '';
-    drawTubes();
-}
-
-async function play() {
-    resetTimeout();
-    const wait = (timeToDelay) => new Promise((resolve) => timeoutHandle = setTimeout(resolve, timeToDelay));
-    
-    ShowPlayControls();
-    while (currentStage < grid.length-1) {
-        await wait(playSpeed);
-        nextStage();
-    }
-    hidePlayControls();
-}
-
-function playPause() {
-    //if playing, pauses, if not, starts 
-    if (isPlaying()) pause();
-    else play();
-}
-
-function isPlaying() {
-    if (document.querySelector('.playControls').style.visibility == 'hidden') {
-        return false; 
-    } else {
-        return true;
-    }
-}
-
-function playFromStart() {
-    resetTimeout();
-    currentStage = -1;
-    play();
-}
-
-function pause() {
-    resetTimeout();
-    hidePlayControls();
-}
-
-function resetTimeout() {
-    clearTimeout(timeoutHandle);
-}
-
-function faster() {
-    // don't want to be able to stop it
-    if (playSpeed > 200) playSpeed-=200;
-    console.log(playSpeed);
-}
-
-function slower() {
-    playSpeed+=200;
-    console.log(playSpeed);
-}
-
-function reset() {
-    playSpeed = 1000;
-    console.log(playSpeed);
-}
-
-function ShowPlayControls() {
-    document.querySelectorAll('.playControls').forEach((el) => el.style.visibility = 'visible');  
-}
-
-function hidePlayControls() {
-    document.querySelectorAll('.playControls').forEach((el) => el.style.visibility = 'hidden');
-}
 
 
 // Next step is to be able to pass a created build to this page and show it.  
-// http.get('../Examples Solved/ball_sort_616_solved.json') //  14 colours x 5, puzzle 616 
-// http.get('../Examples Solved/ballsortSolved.json') //   3 colours x 3
-// http.get('../Examples Solved/ballSortSolved7x4.json') //   7 colours x 4
-// http.get('../Examples Solved/ballSortSolved3x4.json') //   3 colours x 4
-http.get('../Examples Solved/ballSortSolved12x5.json') //   3 colours x 4
-.then(data => mainSolve(data))
-.catch(err => console.log(err)); 
 
-function mainSolve(data) {
-    grid = Object.values(data);
-    ballsPerTube = grid[0][0].length;//getTubeSize()
-    prepareToDraw();
-}
+// http.get('../Examples Solved/ballSortSolved12x5.json') //   3 colours x 4
+// .then(data => mainSolve(data))
+// .catch(err => console.log(err)); 
+
+
 
